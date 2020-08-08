b7186696f6a1834e6c3ea17921573252ee2826a5
Thato Mashego <cjmashego66@gmail.com>
2020-08-08 19:11:54 +0200

    removed comments heroku

--- ntm/settings.py
+++ ntm/settings.py
@@ -131,10 +131,10 @@ AUTH_USER_MODEL = 'accounts.Member'
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = [
- #   os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
 
-#]
+]
 
 # LOGIN
 LOGIN_REDIRECT_URL = 'blog:blog_list'
