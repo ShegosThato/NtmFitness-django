643724dff298908071cda5ea30a950abd4caa9d7
Thato Mashego <cjmashego66@gmail.com>
2020-08-08 19:00:52 +0200

    Added the heroku cli info

--- ntm/settings.py
+++ ntm/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,3 +141,4 @@ LOGIN_REDIRECT_URL = 'blog:blog_list'
 LOGOUT_REDIRECT_URL = 'blog:blog_list'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+django_heroku.settings(locals())
