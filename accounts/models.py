b35242ce42154f87c20a1e9ea55b15f6417033a4
ShegosThato <cjmashego66@gmail.com>
2020-10-03 02:39:44 +0200

    Removed default values in integer fields

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Member(AbstractUser):
-    id_num = models.IntegerField(default=99101015030080)
+    id_num = models.IntegerField(default=991010)
     house_num = models.IntegerField(default=0000)
     street_name = models.CharField(max_length=50)
     town = models.CharField(max_length=50)
