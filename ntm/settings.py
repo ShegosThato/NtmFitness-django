c08d849d1514517654264cd856631ef99efba814
ShegosThato <cjmashego66@gmail.com>
2020-10-03 02:27:55 +0200

    Fixed errors for deployment 2

--- ntm/settings.py
+++ ntm/settings.py
@@ -84,9 +84,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ntm.wsgi.application'
 
 
-#Heroku: Update database config from $DATABASE_URL
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -142,3 +149,6 @@ LOGOUT_REDIRECT_URL = 'blog:blog_list'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 django_heroku.settings(locals())
 
+#Heroku: Update database config from $DATABASE_URL
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
